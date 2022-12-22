44d848138243b5d62f73aec5bcccd71c3ed0f435
Nadatari <dariosting@gmail.com>
2022-12-22 19:56:34 +0100

    Created Counter component, added styles. Redux state needs to change to track counters

--- src/components/ShowTokens.jsx
+++ src/components/ShowTokens.jsx
@@ -4,30 +4,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../features/index";
 import Pagination from "./Pagination";
+import Counters from "./Counters";
 
 const ShowTokens = ({ player }) => {
   const [tokenState, setTokenState] = useState(true);
 
   const trackedTokens = player.tokens;
 
-  const uniqueArray = trackedTokens.filter(function (item, pos) {
-    return trackedTokens.indexOf(item) == pos;
-  });
 
-  var uniq = trackedTokens.reduce(function(a,b){
-    if (a.indexOf(b) < 0 ) a.push(b);
-    return a;
-  },[]);
-  /* Count numbers of elements in an array */
-  const count = {};
-
-  for (const element of trackedTokens) {
-    if (count[element]) {
-      count[element] += 1;
-    } else {
-      count[element] = 1;
-    }
-  }
 
 
 
@@ -35,7 +19,7 @@ const ShowTokens = ({ player }) => {
   // User is currently on this page
   const [currentPage, setCurrentPage] = useState(1);
   // No of Records to be displayed on each page
-  const [recordsPerPage] = useState(10);
+  const [recordsPerPage] = useState(5);
   const indexOfLastRecord = currentPage * recordsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   // Records to be displayed on the current page
@@ -60,7 +44,7 @@ const ShowTokens = ({ player }) => {
       </button>
       {/* Modal */}
       <div
-        className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
+        className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-10"
         hidden={tokenState}
       >
         <div className="relative top-2 m-10 p-2 border bg-white">
@@ -73,7 +57,7 @@ const ShowTokens = ({ player }) => {
           <div className="w-full flex justify-center">
             <div className="grid grid-cols-5 gap-8">
               {trackedTokens &&
-                uniq.map((e) => (
+                currentRecords.map((e) => (
                   <div key={uuidv4()}>
                     {e && (
                       <div className="relative cursor-pointer transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
@@ -83,13 +67,11 @@ const ShowTokens = ({ player }) => {
                           className="w-60"
                         />
 
-                        {count[e] && (
-                          <div className="absolute right-4 top-4 z-10 bg-orange-500 px-2 py-1 rounded-full text-white">
-                            {count[e]}
-                          </div>
-                        )}
+                      
                       </div>
+                      
                     )}
+                    <Counters />
                   </div>
                 ))}
             </div>
